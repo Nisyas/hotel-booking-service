bbf337e45e11694f57ff622fa106c9894016a5d3
Valerii <valeratk32@list.ru>
2025-11-25 13:00:34 +0300

    Создание Main

--- bookings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("create/", views.booking_create, name="booking-create"),
-    path("delete/<int:booking_id>/", views.booking_delete, name="booking-delete"),
-    path("list/", views.booking_list, name="booking-list"),
-]
